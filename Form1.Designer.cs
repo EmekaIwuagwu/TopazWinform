84b6375595f5b7c0ee295b90a855685a5299edde
EmekaIwuagwu <45670176+EmekaIwuagwu@users.noreply.github.com>
2019-01-19 16:04:11 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace SignatureTestExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sigPlusNET1 = new Topaz.SigPlusNET();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sigPlusNET1
+            // 
+            this.sigPlusNET1.BackColor = System.Drawing.Color.White;
+            this.sigPlusNET1.ForeColor = System.Drawing.Color.Black;
+            this.sigPlusNET1.Location = new System.Drawing.Point(12, 12);
+            this.sigPlusNET1.Name = "sigPlusNET1";
+            this.sigPlusNET1.Size = new System.Drawing.Size(507, 137);
+            this.sigPlusNET1.TabIndex = 0;
+            this.sigPlusNET1.Text = "sigPlusNET1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sign";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(428, 158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Save to DB";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter ID : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 204);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(271, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(346, 202);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(173, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Show Image";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(14, 230);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(505, 122);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(251, 158);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(170, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Save Base64 and SigImage";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 364);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sigPlusNET1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Topaz.SigPlusNET sigPlusNET1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignatureTestExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sigPlusNET1.SetTabletState(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sigPlusNET1.ClearTablet();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sigString = sigPlusNET1.GetSigString();
+                // MessageBox.Show(sigString);
+
+                string constring = @"Data Source=DESKTOP-FJBB72F\SQLEXPRESS;Initial Catalog=SignatureCapture;Integrated Security=True";
+                using (SqlConnection con = new SqlConnection(constring))
+                {
+
+                    con.Open();
+                    string query = "insert into SignTable (signature) values (@signature)";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@signature", sigString);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("OK I Am inserted!");
+                        con.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.ToString());
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string constring = @"Data Source=DESKTOP-FJBB72F\SQLEXPRESS;Initial Catalog=SignatureCapture;Integrated Security=True";
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                con.Open();
+                string sql = "select * from SignTable where id =@id";
+                using (SqlCommand cm = new SqlCommand(sql, con))
+                {
+                    cm.Parameters.AddWithValue("@id", textBox1.Text);
+                    try
+                    {
+                        using (SqlDataReader rd = cm.ExecuteReader())
+                        {
+                            if (rd.Read())
+                            {
+                                // byte[] imgData = (byte[])rd["signature"];
+                                //byte[] imgData = Convert.FromBase64String(rd["signature"].ToString());
+                                byte[] imgData = Encoding.ASCII.GetBytes(rd["signature"].ToString());
+                                using (MemoryStream ms = new MemoryStream(imgData))
+                                {
+                                    System.Drawing.Image image = Image.FromStream(ms);
+                                    //image.Save(@"C:\Users\Administrator\Desktop\UserPhoto.jpg");
+                                    pictureBox1.Image = image;
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.ToString());
+                    }
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sigPlusNET1.SetTabletState(0); //tablet off 
+            sigPlusNET1.SetImageXSize(1000);
+            sigPlusNET1.SetImageYSize(300);
+            sigPlusNET1.SetJustifyY(10);
+            sigPlusNET1.SetJustifyX(10);
+            sigPlusNET1.SetJustifyMode(5);
+            sigPlusNET1.SetImagePenWidth(10);
+            sigPlusNET1.SetImageFileFormat(4); //0=bmp, 4=jpg, 6=tif
+            Image sigImage = sigPlusNET1.GetSigImage();
+            String sigBase64 = ImageToBase64(sigImage, ImageFormat.Jpeg);            
+            string sigString = sigPlusNET1.GetSigString();
+
+            if (sigBase64.Length > 2500)
+            {
+                string constring = @"Data Source=DESKTOP-FJBB72F\SQLEXPRESS;Initial Catalog=SignatureProcess;Integrated Security=True";
+                using (SqlConnection con = new SqlConnection(constring))
+                {
+                    con.Open();
+                    try
+                    {
+                        string insertSql = "insert into signatureProcessNew (sigString,base64Sigstring) values (@sigString,@base64Sigstring)";
+                        using (SqlCommand cd = new SqlCommand(insertSql, con))
+                        {
+                            cd.Parameters.AddWithValue("@sigString", sigString);
+                            cd.Parameters.AddWithValue("@base64Sigstring", sigBase64);
+                            cd.ExecuteNonQuery();
+                            MessageBox.Show("Data Saved!");
+                            con.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.ToString());
+                    }
+                }
+            }
+        }
+
+        public string ImageToBase64(Image image, ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Convert Image to byte[] 
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+                // Convert byte[] to Base64 String 
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
+        }
+    }
+}
